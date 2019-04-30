c57e7b20e367ee4f577e81e1bb6f89e4071a1c1c
yuchanmo <mojjijji@gmail.com>
2019-04-30 11:25:09 +0900

    save

---
+++ ch5.py
@@ -0,0 +1,8 @@
+# import tensorflow as tf
+
+
+# from tensorflow.examples.tutorials.mnist import input_data
+# mnist = input_data.read_data_sets('./mnist/data/',one_hot=True)
+
+import numpy as np
+a = np.random.randint(1,100,size=(3,25,28))
